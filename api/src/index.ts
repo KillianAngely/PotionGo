cbcbfad81addfd0359ba73d601ecb9a993c2b366
killian <killian.angely@outlook.fr>
2025-10-14 16:01:42 +0200

    vanilla

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,17 +1,17 @@
-import express  from 'express';
-import type {  Request,  Response } from 'express';
-import cors  from 'cors';
+import express from "express"
+import type { Request, Response } from "express"
+import cors from "cors"
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.get('/api/health', (_req: Request, res: Response) => {
-  res.json({ message: 'Hello from PotionGo API!' });
-});
+app.get("/api/health", (_req: Request, res: Response) => {
+  res.json({ message: "Hello from PotionGo API!" })
+})
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT} 🌍`);
-});
+  console.log(`Server is running on http://localhost:${PORT} 🌍`)
+})
