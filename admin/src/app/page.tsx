f4f10667c34506783cc7d1077d1262ff02dafa4d
Youri2310 <97401240+Youri2310@users.noreply.github.com>
2025-10-15 09:48:05 +0200

    feat(admin): initialize app next

---
+++ admin/src/app/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import styles from "./page.module.css";
+
+export default function Home() {
+  const [message, setMessage] = useState("Chargement...");
+
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/health`)
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => {
+        console.error("Erreur API :", err);
+        setMessage("Erreur de connexion à l’API");
+      });
+  }, []);
+
+  return (
+    <div className={styles.page}>
+      <h1>Statut de l'API :</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
