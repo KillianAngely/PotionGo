9af0d42e869148e777b7f3a1c68b6f57d6276b49
killian <killian.angely@outlook.fr>
2026-01-05 21:25:53 +0100

    feat(app): add signOut

--- app/app/src/main/java/com/example/potiongo/ui/screen/HomeScreen.kt
+++ app/app/src/main/java/com/example/potiongo/ui/screen/HomeScreen.kt
@@ -1,28 +1,40 @@
 package com.example.potiongo.ui.screen
 
-import androidx.compose.foundation.layout.Arrangement
+
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
+
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+
 import com.example.potiongo.ui.theme.PotionGoTheme
 
 
+
 @Composable
-fun HomeScreen() {
+fun HomeScreen(viewModel: HomeViewModel = viewModel()) {
+
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "Home screen :-) !",
-            textAlign = TextAlign.Center,
-        )
+
+        Button(
+            onClick = {
+                viewModel.signOut()
+            }
+        ) {
+            Text(
+                text = "SignOut",
+                textAlign = TextAlign.Center,
+            )
+        }
     }
 }
 
---
+++ app/app/src/main/java/com/example/potiongo/ui/screen/HomeViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.potiongo.ui.screen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.potiongo.services.AuthService
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class HomeViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(HomeUiState())
+    val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
+
+
+    fun signOut(){
+        viewModelScope.launch {
+            val auth = AuthService(FirebaseAuth.getInstance())
+            auth.signOut()
+        }
+    }
+}
+
+data class HomeUiState(
+    val test : String = "TEST"
+)
