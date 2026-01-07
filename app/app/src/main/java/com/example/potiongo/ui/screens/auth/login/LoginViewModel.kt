d8a76da0f3546740417a0965203f68ae19ce6e61
killian <killian.angely@outlook.fr>
2026-01-07 19:49:42 +0100

    feat(app): add verifEmail

--- app/app/src/main/java/com/example/potiongo/ui/screens/auth/login/LoginViewModel.kt
+++ app/app/src/main/java/com/example/potiongo/ui/screens/auth/login/LoginViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.potiongo.ui.screens.signup
+package com.example.potiongo.ui.screens.auth.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -10,10 +10,9 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-
-class SignUpViewModel : ViewModel() {
-    private val _uiState = MutableStateFlow(SignUpUiState())
-    val uiState: StateFlow<SignUpUiState> = _uiState.asStateFlow()
+class LoginViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(LoginUiState())
+    val uiState: StateFlow<LoginUiState> = _uiState.asStateFlow()
 
     fun updateEmail(email: String) {
         _uiState.update { currentState ->
@@ -27,26 +26,15 @@ class SignUpViewModel : ViewModel() {
         }
     }
 
-    fun updateConfirmPassword(confirmPassword: String) {
-        _uiState.update { currentState ->
-            currentState.copy(confirmPassword = confirmPassword)
-        }
-    }
-
-
-    fun signUp() {
+    fun login() {
         viewModelScope.launch {
             val auth = AuthService(FirebaseAuth.getInstance())
-            auth.signUp(_uiState.value.email, _uiState.value.password)
+            auth.login(_uiState.value.email, _uiState.value.password)
         }
     }
-
 }
 
-
-data class SignUpUiState(
+data class LoginUiState(
     val email: String = "",
-    val password: String = "",
-    val confirmPassword : String = "",
-    val hasError:  Boolean = false
+    val password: String = ""
 )
