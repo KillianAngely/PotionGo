6e2704cf849e4510eace1a9c779359b3bff1cbba
killian <killian.angely@outlook.fr>
2026-01-11 13:41:28 +0100

    feat(app): set custom claims

--- admin/config/firebase-admin.js
+++ admin/config/firebase-admin.js
@@ -1,12 +1,11 @@
-import admin from "firebase-admin";
+import admin from "firebase-admin"
 
-const serviceAccount = require("../client_service_account.json");
+const serviceAccount = require("../client_service_account.json")
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://potiongo-default-rtdb.europe-west1.firebasedatabase.app"
-  });
+  })
 }
 
-export default admin;
+export default admin
