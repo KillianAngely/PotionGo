ccdf399960aceb5e694614e0a429cb8014f787e3
Youri2310 <youriemmanue@gmail.com>
2026-01-16 15:08:32 +0100

    feat(admin): user repository

--- admin/src/app/00_INFRA/types/User.ts
+++ admin/src/app/00_INFRA/types/User.ts
@@ -1,3 +1,18 @@
-type User = {
-  uid: string
+export enum UserAdminRole {
+    ADMIN = "ADMIN",
+}
+
+export enum UserClientRole {
+    CUSTOMER = "CUSTOMER",
+    DRIVER = "DRIVER",
+}
+
+export type UserRole = UserAdminRole | UserClientRole;
+
+export type User = {
+    uid: string
+    email: string
+    firstName: string
+    lastName: string
+    role: UserRole
 }
