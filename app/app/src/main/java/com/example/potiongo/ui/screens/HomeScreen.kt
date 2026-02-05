a2c87d27b8a6b4b35510f6eb6072684fee0aebde
killian <killian.angely@outlook.fr>
2026-02-05 14:14:04 +0100

    refeactor(app): HomePage

--- app/app/src/main/java/com/example/potiongo/ui/screens/HomeScreen.kt
+++ app/app/src/main/java/com/example/potiongo/ui/screens/HomeScreen.kt
@@ -1,10 +1,18 @@
 package com.example.potiongo.ui.screens
 
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -15,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.flowWithLifecycle
@@ -22,6 +31,7 @@ import com.example.potiongo.ui.theme.PotionGoTheme
 import kotlinx.coroutines.flow.filter
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     homeViewModel: HomeViewModel = hiltViewModel(),
@@ -35,21 +45,41 @@ fun HomeScreen(
                 currentOnUserLogIn()
         }
     }
-
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-
-        Button(
-            onClick = {
-                homeViewModel.signOut()
-            }
-        ) {
-            Text(
-                text = "SignOut",
-                textAlign = TextAlign.Center,
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Top app bar")
+                }
             )
+        },
+        bottomBar = {
+            BottomAppBar(){
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "Bottom app bar",
+                )
+            }
+        },
+    ) { innerPadding ->
+        Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding),
+        verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+            Button(
+                onClick = {
+                    homeViewModel.signOut()
+                }
+            ) {
+                Text(
+                    text = "SignOut",
+                    textAlign = TextAlign.Center,
+                )
+            }
         }
     }
 }
