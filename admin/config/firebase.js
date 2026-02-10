04b51d6a9732b9fca62453536b6baee8fcb9a0c7
Youri2310 <youriemmanue@gmail.com>
2026-02-10 14:14:06 +0100

    fix:  client-side storage upload for products

--- admin/config/firebase.js
+++ admin/config/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 import { getDatabase } from "firebase/database"
+import { getStorage } from "firebase/storage"
 
 // TODO: Add SDKs for Firebase products that you want to use
 
@@ -20,3 +21,4 @@ export const app = initializeApp(firebaseConfig)
 export const firestore = getFirestore(app)
 export const realtime = getDatabase(app)
 export const auth = getAuth(app)
+export const storage = getStorage(app)
