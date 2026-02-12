a150194f696b172907118c19c6798c889c5ecb44
Youri2310 <youriemmanue@gmail.com>
2026-02-12 11:15:14 +0100

    fix(auth): resolve authentication and session cookie issues and session cookie race condition

--- admin/src/middleware.ts
+++ admin/src/middleware.ts
@@ -39,6 +39,14 @@ export function middleware(_request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico|css|js|map)$).*)",
+    /*
+     * Match all request paths except:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public files (images, etc)
+     */
+    "/((?!api/|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico|css|js|map)$).*)",
   ],
 }
